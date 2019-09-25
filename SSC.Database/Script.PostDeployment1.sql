fe235e96e16ad798988ea907920e6c34e016df50
Christian Guzman <cguzman@resolvit.com>
2019-09-25 19:44:16 -0300

    Roles y permisos por script

--- SSC.Database/Script.PostDeployment1.sql
+++ SSC.Database/Script.PostDeployment1.sql
@@ -56,7 +56,234 @@ LEFT  JOIN	Role R
 		ON	data.Name = r.Name
 WHERE		r.Name IS NULL
 
--- Initial Data: Pricing Plans
+-- Satellite Data: Permissions
+INSERT Permission (
+	Code, 
+	Name,
+	CreatedDate,
+	UpdatedDate
+)
+SELECT
+	Code = aux.Code, 
+	Name = aux.Name,
+	CreatedDate = GETUTCDATE(),
+	UpdatedDate = GETUTCDATE()
+FROM (
+	SELECT Code = 'CLIENT_BILLING_MANAGEMENT', Name = 'Gestión de Facturación de Clientes' UNION ALL
+	SELECT Code = 'CLIENT_MANAGEMENT', Name = 'Gestión de Clientes' UNION ALL
+	SELECT Code = 'LANGUAGES_MANAGEMENT', Name = 'Gestión de Idiomas de la plataforma' UNION ALL
+	SELECT Code = 'NEWS_MANAGEMENT', Name = 'Gestión de Noticias' UNION ALL
+	SELECT Code = 'PLATFORM_ADMIN', Name = 'El usuario es Administrador de la plataforma y no puede ser deshabilitado/eliminado.' UNION ALL
+	SELECT Code = 'PLATFORM_BACKUP', Name = 'Puede accionar un resguardo de base de datos.' UNION ALL
+	SELECT Code = 'PLATFORM_RESTORE', Name = 'Puede accionar una restauración de base de datos.' UNION ALL
+	SELECT Code = 'ROLES_MANAGEMENT', Name = 'Gestión de Roles de la plataforma' UNION ALL
+	SELECT Code = 'ROLES_REPORT', Name = 'Acceso al Reporte de Roles de la plataforma' UNION ALL
+	SELECT Code = 'UNIT_OF_MEASURE_MANAGEMENT', Name = 'Gestión de Unidades de Medida' UNION ALL
+	SELECT Code = 'USERS_MANAGEMENT', Name = 'Gestión de Usuarios de la plataforma' UNION ALL
+	SELECT Code = 'USERS_REPORT', Name = 'Acceso al Reporte de Usuarios de la plataforma' UNION ALL
+	SELECT Code = 'MEMBER_MANAGEMENT', Name = 'Gestión de Usuarios Miembro' UNION ALL
+	SELECT Code = 'MEMBER_REPORT', Name = 'Acceso al Reporte de Miembros' UNION ALL
+	SELECT Code = 'PATIENTS_MANAGEMENT', Name = 'Gestión de Pacientes' UNION ALL
+	SELECT Code = 'PAYMENT_METHOD_MANAGEMENT', Name = 'Gestión de Forma de Pago' UNION ALL
+	SELECT Code = 'RUN_EXECUTION_CANCEL', Name = 'Cancelación de lotes de ejecución' UNION ALL
+	SELECT Code = 'RUN_EXECUTION_PRIMARY', Name = 'Generación y Ejecución primaria de lotes de ejecución' UNION ALL
+	SELECT Code = 'RUN_EXECUTION_QA', Name = 'Control final de calidad de lotes de ejecución' UNION ALL
+	SELECT Code = 'RUN_EXECUTION_QC', Name = 'Control de calidad de lotes de ejecución' UNION ALL
+	SELECT Code = 'SAMPLE_FUNCTION_MANAGEMENT', Name = 'Gestión de Funciones de Muestra' UNION ALL
+	SELECT Code = 'SAMPLE_FUNCTION_REPORT', Name = 'Acceso al Reporte de Funciones de Muestra' UNION ALL
+	SELECT Code = 'SAMPLE_MANAGEMENT', Name = 'Gestión de Muestras de Paciente y de Control' UNION ALL
+	SELECT Code = 'SAMPLE_TYPE_MANAGEMENT', Name = 'Gestión de Tipos de Muestra' UNION ALL
+	SELECT Code = 'SAMPLE_TYPE_REPORT', Name = 'Acceso al Reporte de Tipos de Muestra' UNION ALL
+	SELECT Code = 'USERS_INVITE', Name = 'Puede invitar a nuevos miembros a la plataforma.' UNION ALL
+	SELECT Code = 'WORK_ORDER_CREATE', Name = 'Creación de Órdenes de Trabajo' UNION ALL
+	SELECT Code = 'WORK_ORDER_EXECUTE', Name = 'Ejecución de Órdenes de Trabajo' UNION ALL
+	SELECT Code = 'WORK_ORDER_REPORT', Name = 'Listar órdenes de trabajo'
+) AS aux
+LEFT  JOIN	Permission P
+		ON	aux.Code = p.Code
+WHERE		p.Code IS NULL
+
+-- Satellite Data: Initial Roles
+INSERT Role (
+	Name,
+	IsPlatformRole,
+	IsEnabled,
+	CreatedDate,
+	UpdatedDate
+)
+SELECT
+	Name = aux.Name,
+	IsPlatformRole = aux.IsPlatformRole,
+	IsEnabled = 1,
+	CreatedDate = GETUTCDATE(),
+	UpdatedDate = GETUTCDATE()
+FROM (
+	SELECT Name = 'Usuario No Registrado', IsPlatformRole = 0 UNION ALL
+	SELECT Name = 'Administrador de Sistema', IsPlatformRole = 1 UNION ALL
+	SELECT Name = 'Científico Ejecutor', IsPlatformRole = 0 UNION ALL
+	SELECT Name = 'Científico Auditor', IsPlatformRole = 0 UNION ALL
+	SELECT Name = 'Controlador de Calidad', IsPlatformRole = 0 UNION ALL
+	SELECT Name = 'Administrador de Cliente', IsPlatformRole = 0 
+) AS aux
+LEFT  JOIN	Role R
+		ON	aux.Name = r.Name
+WHERE		r.Name IS NULL
+
+-- Role to Permissions - Administrador de Sistema
+IF(NOT EXISTS(SELECT TOP 1 1 FROM RolePermission RP INNER JOIN Role R ON rp.RoleId = r.Id WHERE r.Name = 'Administrador de Sistema'))
+BEGIN
+	INSERT RolePermission (
+		RoleId,
+		PermissionId,
+		CreatedDate,
+		UpdatedDate
+	)
+	SELECT
+		RoleId = r.Id,
+		PermissionId = p.Id,
+		CreatedDate = GETUTCDATE(),
+		UpdatedDate = GETUTCDATE()
+	FROM		Role R
+	CROSS JOIN	Permission P
+	WHERE		R.Name = 'Administrador de Sistema'
+	AND			P.Code IN (
+		'CLIENT_BILLING_MANAGEMENT',
+		'CLIENT_MANAGEMENT',
+		'LANGUAGES_MANAGEMENT',
+		'NEWS_MANAGEMENT',
+		'PLATFORM_ADMIN',
+		'PLATFORM_BACKUP',
+		'PLATFORM_RESTORE',
+		'ROLES_MANAGEMENT',
+		'ROLES_REPORT',
+		'UNIT_OF_MEASURE_MANAGEMENT',
+		'USERS_MANAGEMENT',
+		'USERS_REPORT'
+	)
+END
+
+-- Role to Permissions - Científico Ejecutor
+IF(NOT EXISTS(SELECT TOP 1 1 FROM RolePermission RP INNER JOIN Role R ON rp.RoleId = r.Id WHERE r.Name = 'Científico Ejecutor'))
+BEGIN
+	INSERT RolePermission (
+		RoleId,
+		PermissionId,
+		CreatedDate,
+		UpdatedDate
+	)
+	SELECT
+		RoleId = r.Id,
+		PermissionId = p.Id,
+		CreatedDate = GETUTCDATE(),
+		UpdatedDate = GETUTCDATE()
+	FROM		Role R
+	CROSS JOIN	Permission P
+	WHERE		R.Name = 'Científico Ejecutor'
+	AND			P.Code IN (
+		'RUN_EXECUTION_CANCEL',
+		'RUN_EXECUTION_PRIMARY',
+		'SAMPLE_FUNCTION_REPORT',
+		'SAMPLE_TYPE_REPORT',
+		'SAMPLE_MANAGEMENT',
+		'WORK_ORDER_CREATE',
+		'WORK_ORDER_EXECUTE',
+		'WORK_ORDER_REPORT'
+	)
+END
+
+-- Role to Permissions - Científico Auditor
+IF(NOT EXISTS(SELECT TOP 1 1 FROM RolePermission RP INNER JOIN Role R ON rp.RoleId = r.Id WHERE r.Name = 'Científico Auditor'))
+BEGIN
+	INSERT RolePermission (
+		RoleId,
+		PermissionId,
+		CreatedDate,
+		UpdatedDate
+	)
+	SELECT
+		RoleId = r.Id,
+		PermissionId = p.Id,
+		CreatedDate = GETUTCDATE(),
+		UpdatedDate = GETUTCDATE()
+	FROM		Role R
+	CROSS JOIN	Permission P
+	WHERE		R.Name = 'Científico Auditor'
+	AND			P.Code IN (
+		'RUN_EXECUTION_CANCEL',
+		'RUN_EXECUTION_QA',
+		'SAMPLE_FUNCTION_REPORT',
+		'SAMPLE_TYPE_REPORT',
+		'SAMPLE_MANAGEMENT',
+		'WORK_ORDER_REPORT'
+	)
+END
+
+-- Role to Permissions - Controlador de Calidad
+IF(NOT EXISTS(SELECT TOP 1 1 FROM RolePermission RP INNER JOIN Role R ON rp.RoleId = r.Id WHERE r.Name = 'Controlador de Calidad'))
+BEGIN
+	INSERT RolePermission (
+		RoleId,
+		PermissionId,
+		CreatedDate,
+		UpdatedDate
+	)
+	SELECT
+		RoleId = r.Id,
+		PermissionId = p.Id,
+		CreatedDate = GETUTCDATE(),
+		UpdatedDate = GETUTCDATE()
+	FROM		Role R
+	CROSS JOIN	Permission P
+	WHERE		R.Name = 'Controlador de Calidad'
+	AND			P.Code IN (
+		'RUN_EXECUTION_CANCEL',
+		'RUN_EXECUTION_QC',
+		'SAMPLE_FUNCTION_REPORT',
+		'SAMPLE_TYPE_REPORT',
+		'SAMPLE_MANAGEMENT',
+		'WORK_ORDER_REPORT'
+	)
+END
+
+-- Role to Permissions - Administrador de Cliente
+IF(NOT EXISTS(SELECT TOP 1 1 FROM RolePermission RP INNER JOIN Role R ON rp.RoleId = r.Id WHERE r.Name = 'Administrador de Cliente'))
+BEGIN
+	INSERT RolePermission (
+		RoleId,
+		PermissionId,
+		CreatedDate,
+		UpdatedDate
+	)
+	SELECT
+		RoleId = r.Id,
+		PermissionId = p.Id,
+		CreatedDate = GETUTCDATE(),
+		UpdatedDate = GETUTCDATE()
+	FROM		Role R
+	CROSS JOIN	Permission P
+	WHERE		R.Name = 'Administrador de Cliente'
+	AND			P.Code IN (
+		'MEMBER_MANAGEMENT',
+		'MEMBER_REPORT',
+		'PATIENTS_MANAGEMENT',
+		'PAYMENT_METHOD_MANAGEMENT',
+		'RUN_EXECUTION_CANCEL',
+		'RUN_EXECUTION_PRIMARY',
+		'RUN_EXECUTION_QA',
+		'RUN_EXECUTION_QC',
+		'SAMPLE_FUNCTION_MANAGEMENT',
+		'SAMPLE_FUNCTION_REPORT',
+		'SAMPLE_MANAGEMENT',
+		'SAMPLE_TYPE_MANAGEMENT',
+		'SAMPLE_TYPE_REPORT',
+		'USERS_INVITE',
+		'WORK_ORDER_CREATE',
+		'WORK_ORDER_EXECUTE',
+		'WORK_ORDER_REPORT'
+	)
+END
+
+-- Satellite Data: Pricing Plans
 INSERT PricingPlan (
 	Code,
 	Name,
